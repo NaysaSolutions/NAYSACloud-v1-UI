f63986244fdfb0dea15e9354d2d319ed8509d73b
Merie Anjeaneth Alarcon <aalarcon.naysa@gmail.com>
2025-05-19 20:26:20 +0800

    Branch UI: Add Delete, Edit, Export, Guide features

--- src/NAYSA Cloud/Global/reftable.js
+++ src/NAYSA Cloud/Global/reftable.js
@@ -20,3 +20,48 @@ export const reftables = {
 
 
   };
+
+  
+  export const reftablesVideoGuide = {
+    
+    // General Ledger Module
+    Branch: "https://www.youtube.com/watch?v=e5gBnrL-3u4&list=PLfNvt59xJjIgoEopcrnnG9fWfz76EMIxO&index=5&t=9s",
+    PCV: "https://www.youtube.com/watch?v=VsITqPlYIjM&ab_channel=Learn%40NAYSA",
+
+    // Accounts Payable Module
+    APV: "https://www.youtube.com/watch?v=e5gBnrL-3u4&list=PLfNvt59xJjIgoEopcrnnG9fWfz76EMIxO&index=5&t=9s",
+    APDM: "https://youtu.be/ZzFMbTBLp-I",
+    APCM: "https://youtu.be/ULTdHwSylLs",
+    CV: "https://www.youtube.com/watch?v=x8CsG1pHSM8&ab_channel=Learn%40NAYSA",
+
+    // Accounts Receivable Module
+    SI: "Sales Invoice Transaction",
+    SVI: "Service Invoice Transaction",
+    ARDM: "AR Debit Memo Voucher Transaction",
+    ARCM: "AR Credit Memo Voucher Transaction",
+    CR: "Collection Receipt Transaction",
+    SOA: "Statement of Account Transaction",
+
+  };
+
+  export const reftablesPDFGuide = {
+    
+    // General Ledger Module
+    Branch: "/public/Guide/NAYSA AP Accounts Payable Voucher.pdf",
+    PCV: "/public/Guide/NAYSA GL Petty Cash Voucher.pdf",
+
+    // Accounts Payable Module
+    APV: "/public/Guide/NAYSA AP Accounts Payable Voucher.pdf",
+    APDM: "/public/Guide/NAYSA AP Debit Memo.pdf",
+    APCM: "/public/Guide/NAYSA AP Credit Memo.pdf",
+    CV: "/public/Guide/NAYSA AP Check Voucher.pdf",
+
+    // Accounts Receivable Module
+    SI: "Sales Invoice Transaction",
+    SVI: "Service Invoice Transaction",
+    ARDM: "AR Debit Memo Voucher Transaction",
+    ARCM: "AR Credit Memo Voucher Transaction",
+    CR: "Collection Receipt Transaction",
+    SOA: "Statement of Account Transaction",
+
+  };
